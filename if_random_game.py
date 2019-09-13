60732a76d1efab408ba62096acc35c3f290b68db
Varya Dementeva <varya.dementjeva@gmail.com>
2019-09-13 16:08:13 +0300

    init commit

--- if_random_game.py
+++
@@ -1,9 +0,0 @@
-import random
-x = random.randint(1,4)
-y = int(input("Я загадал число от 1 до 4. Ты думаешь, что это число: "))
-if x == y:
-    print("Ты победил!!")
-elif x<y:
-    print("Нет, мое число меньше...")
-else:
-    print("Нет, мое число больше...")
